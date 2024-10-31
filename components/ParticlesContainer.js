767ba28d34a7b802c79fb6ab8f8bb82f4ff88f2a
BABARSHAH786 <159919873+BABARSHAH786@users.noreply.github.com>
2024-10-31 19:44:23 +0500

    new code add

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,5 +1,50 @@
+import {Particles} from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import React,{useCallback} from 'react';
 const ParticlesContainer = () => {
-  return <div>Particles Container</div>;
+  //init
+  const particleInit = useCallback(async (engine )=>{
+
+  },[]);
+  const particleLoaded =useCallback(async()=>{},[]);
+
+  return (
+  <Particles ClassName='w-full h-full absolute translate-2-0' id='tsparticles' init={particleInit} loaded={particleLoaded}
+  options={{
+    fullScreen:{enable:false},
+    background:{
+      color:{
+        value:'',
+      },
+    },
+    fpsLimit:120,
+    interactivity:{
+      events:{
+        onClick:{
+          enable:false,
+          mode:'push',
+        },
+        onhover:{
+          enable:true,
+          mode:'repulse'
+        },
+        resize:true,
+      },
+      modes:{
+        push:{quantity:90},
+        repulse:{
+          distance:200,
+          duration:0.4,
+        },
+        particles:{
+          color:{
+            value:'#e68e2e'
+          }
+        }
+      },
+    }
+  }} />
+  )
 };
 
 export default ParticlesContainer;
